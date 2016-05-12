c5c67e6a281176eb94214366ad2d9e185d26dec8
Kiril Vatev <vatev.1@gmail.com>
2016-05-12 17:50:52 -0400

    fixing require path in cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* jshint node: true */
 
-var unstyle = require('../unstyle.js');
+var unstyle = require('../lib/unstyle.js');
 
 unstyle.stream(process.stdin, process.stdout);
