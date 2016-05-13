0206e1d572b7e298e64e5c7cf51e678522aed664
Kiril Vatev <vatev.1@gmail.com>
2016-05-12 21:10:23 -0400

    testing that all styles in chalk get properly unstyled

--- test/unstyle.test.js
+++ test/unstyle.test.js
@@ -7,43 +7,101 @@ var chalk = require('chalk');
 
 var lib = require('../lib/unstyle.js');
 
-var STR = 'red';
-var COLORED_STR = (function(str) {
+function styleStr(str, func) {
     return str.split().map(function(char) {
-        return chalk.red(char);
+        return func(char);
     }).join('');
-})(STR);
+}
 
 describe('[unstyle]', function() {
     describe('#stream', function() {
+        var STR = 'red';
+        var STYLED = styleStr(STR, chalk.red.bind(chalk));
+        
+        it('pipes content through to the output', function(done) {
+            var input = through();
+            var output = through();
+            
+            lib.stream(input, output);
+            
+            output.pipe(es.wait(function(err, data) {
+                data = data.toString();
+                
+                expect(data).to.equal(STR + STR);
+                
+                done();
+            }));
+            
+            input.write(STR);
+            input.write(STR);
+            input.end();
+        });
+        
         it('writes unstyled output to the output stream', function(done) {
             var input = through();
             var output = through();
             
             lib.stream(input, output);
             
-            var dataArr = [];
-            
             output.pipe(es.wait(function(err, data) {
                 data = data.toString();
                 
-                expect(data).to.not.equal(COLORED_STR);
+                expect(data).to.not.equal(STYLED);
                 expect(data).to.equal(STR);
                 
                 done();
             }));
             
-            input.write(COLORED_STR);
+            input.write(STYLED);
             input.end();
         });
     });
     
     describe('#string', function() {
-        it('returns an unstyled string', function() {
-            var val = lib.string(COLORED_STR);
+        
+        // All the styles listed in the chalk help
+        var styles = [
+            'reset',    
+            'bold', 
+            'dim',  
+            'italic',   
+            'underline',    
+            'inverse',  
+            'hidden',   
+            'strikethrough',    
+            'black',    
+            'red',  
+            'green',    
+            'yellow',   
+            'blue', 
+            'magenta',  
+            'cyan', 
+            'white',    
+            'gray', 
+            'bgBlack',  
+            'bgRed',    
+            'bgGreen',  
+            'bgYellow', 
+            'bgBlue',   
+            'bgMagenta',    
+            'bgCyan',   
+            'bgWhite',  
+        ];
+        
+        styles.forEach(function(name) {
+            var func = chalk[name].bind(chalk);
             
-            expect(val).to.not.equal(COLORED_STR);
-            expect(val).to.equal(STR);
+            var STR = 'cat';
+            var STYLED = styleStr(STR, func);
+            
+            it('returns an unstyled string when styled as ' + name, function() {
+                var val = lib.string(STYLED);
+                
+                expect(STR).to.not.equal(STYLED);
+                expect(val).to.not.equal(STYLED);
+                expect(val).to.equal(STR);
+            });
         });
+        
     });
 });
