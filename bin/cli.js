8aa15c77bf2b64b642c2c3dd2e5e4981fc2afde9
Kiril Vatev <vatev.1@gmail.com>
2016-06-14 16:58:20 -0400

    adding -v and --version to the cli
    #1

--- bin/cli.js
+++ bin/cli.js
@@ -3,4 +3,20 @@
 
 var unstyle = require('../lib/unstyle.js');
 
+function arrContains(arr, val) {
+    return arr.indexOf(val) > -1;
+}
+
+function checkVersionCommand() {
+    var argv = [].slice.call(process.argv);
+    return arrContains(argv, '-v') ||
+        arrContains(argv, '--version');
+}
+
+if (checkVersionCommand()) {
+    var pkg = require('../package.json');
+    console.log('v%s', pkg.version);
+    process.exit(0);
+}
+
 unstyle.stream(process.stdin, process.stdout);
